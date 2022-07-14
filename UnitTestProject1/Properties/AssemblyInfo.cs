f89ac44852f38f51e88f8b6b91465201deb26b57
Pradeep <Pradeepjuloori2021@gmail.com>
2022-07-14 12:49:34 +0530

    commit on 14/7/2022

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f6a57b34-48e7-4f8f-acfb-c6dafe3137fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [TestInitialize]
+        public void TestIntialize()
+        {            
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.google.co.in/";
+            //FuzzBuzzLogic();
+            System.Threading.Thread.Sleep(2000);
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {            
+
+            //click on Gmail link
+            driver.FindElement(By.XPath("//a[text()='Gmail']")).Click();
+            System.Threading.Thread.Sleep(2000);
+            driver.FindElement(By.XPath("//a[text()='Sign in']")).Click();
+            System.Threading.Thread.Sleep(2000);
+            IWebElement nameInput = driver.FindElement(By.XPath("//input[@name='identifier']"));
+            if (nameInput.Displayed)
+            {
+                nameInput.SendKeys("test@gmail.com");
+            }
+
+            //click on next button
+            driver.FindElement(By.XPath("//span[text()='Next']/..")).Click();
+            System.Threading.Thread.Sleep(2000);
+        }
+        [TestCleanup]        
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
